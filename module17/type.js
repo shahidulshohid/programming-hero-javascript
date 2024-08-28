0d0af89b943c8acdb8857ec0af4bfc4c588f2b49
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-29 01:41:31 +0600

    my typeof practice is done

---
+++ module17/type.js
@@ -0,0 +1,18 @@
+// number 
+var num = 300;
+console.log(num);
+console.log(typeof num)
+
+// string 
+var name = 'shahidul islam';
+console.log(name);
+console.log(typeof name);
+
+//boolean
+var isRich = true;
+console.log(isRich);
+console.log(typeof isRich);
+
+
+
+
