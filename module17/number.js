d12fda3fd7acfff5889fb6e9e619bfc752189478
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-29 03:34:47 +0600

    number practice is done

---
+++ module17/number.js
@@ -0,0 +1,15 @@
+var orangePrice = 20;
+var chocolate = 0.5;
+
+// var applePrice = parseInt('22.5')
+var applePrice = parseFloat('22.5')
+
+// console.log(applePrice)
+
+var first = 0.1;
+var second = 0.2;
+var total = first + second;
+
+console.log(total);
+console.log(total.toFixed(1));
+console.log(parseFloat(total.toFixed(1)));
