d73b57673e97197125a4cead15dcc88d88d13779
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-29 00:52:46 +0600

    variable practice is done

--- module17/variable.js
+++ module17/variable.js
@@ -1,6 +1,21 @@
-var weight = 40;
-var number
-console.log(number);
-var money = 500;
- money=800; 
-console.log(money);
+// var weight = 40;
+// var number
+// console.log(number);
+// var money = 500;
+//  money=800; 
+// console.log(money);
+
+
+// var car = "red car";
+// car = "green car"
+// console.log(car)
+
+// let car = "blue car";
+//let car = "yellow car"; {wrong declare for let}
+//     car = "green car";
+// console.log(car);
+
+const car = "yellow car";
+// const car = "green car"; {wrong declare for const}
+// car = "green car" { this is also wrong assign for const}
+console.log(car)
