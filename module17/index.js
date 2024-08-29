c9ae02b497b34c2fce0087d4d295199f46161955
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-29 17:16:35 +0600

    promt practice is done

---
+++ module17/index.js
@@ -0,0 +1,13 @@
+// Gets user input
+
+var name = prompt("What is your name?");
+var num = prompt("What is your favorite number? ");
+
+// Uses user input to print out information
+console.log("Hello " + name + "!");
+console.log(num + "?! That's my favorite number too!");
+var name = document.getElementById('#name').innerHTML = name;
+var name = document.getElementById('#num')
+// Prints out the variable type
+console.log("Name is a " + typeof name);
+console.log("Num is a " + typeof num);
