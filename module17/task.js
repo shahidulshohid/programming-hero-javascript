d499b121a24f25814f5eeda0b554e3500ac681af
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-29 14:35:50 +0600

    module17 task js practice

---
+++ module17/task.js
@@ -0,0 +1,44 @@
+// 1 number task
+
+// var totalCash = 1000;
+// var totalCost = 700;
+// var marketReturn = 1000 - 700;
+// console.log (marketReturn)
+
+// // 2 number task
+// var math = 75.25;
+// var biology = 65;
+// var chemistry = 80;
+// var physics = 35.45;
+// var bangla = 99.5;
+
+// var totalMarks = math + biology + chemistry + physics + bangla;
+// console.log(totalMarks);
+
+// var averageMarks = totalMarks / 5;
+// console.log(averageMarks);
+// // we can use here toFixed () to get fload number after .
+// console.log(averageMarks.toFixed(2));
+
+// // 3 number task
+
+// var mainNumber = 119;
+// var devider = 5;
+// var remainder = mainNumber % devider;
+// console.log(remainder)
+
+// // 4 number task 
+// var value = 42;
+// console.log(isNaN(value));
+// var value = "shahdiul islam";
+// console.log(isNaN(value));
+// var value = NaN;
+// console.log(isNaN(value));
+// var value;
+// console.log(isNaN(value))
+
+// //5 number task
+// const a = isNaN('11');
+// console.log(a);
+const a = isNaN(2-10);
+console.log(a)
