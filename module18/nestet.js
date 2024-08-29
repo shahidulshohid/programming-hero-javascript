3c4a3645ffa2f18eeac57215b4917379077496d4
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-30 00:51:25 +0600

    nestet practice is done

---
+++ module18/nestet.js
@@ -0,0 +1,47 @@
+// const money = -10;
+
+// if(money > 300){
+//     console.log("bondhu you are rich person");
+// }
+// else {
+//     if(money > 100){
+//         console.log('bondhu you are not rich or poor');
+//     }
+//     else {
+//         if (money > 0){
+//             console.log('bondhu tu kola kha calcium bara');
+//         }
+//         else{
+//             console.log('tui amar bondhu na');
+            
+//         }
+//     }
+// }
+
+const age = 12;
+
+if(age > 28){
+    console.log('your are my granpa');
+}
+else {
+    if(age > 25){
+        console.log('your are my frind');
+        
+    }
+    else {
+        if(age > 20) {
+            console.log('your are not my friend');
+            
+        }
+        else {
+            if(age > 15) {
+                console.log('you are my younger brother');
+                
+            }
+            else {
+                console.log('you are nothing to me');
+                
+            }
+        }
+    }
+}
