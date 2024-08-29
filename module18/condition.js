6b2deda5ad6da40089f79002403e3c6d6565cfb8
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-29 23:08:48 +0600

    condition practice is done

---
+++ module18/condition.js
@@ -0,0 +1,19 @@
+const salary = 25000;
+const isBcs = true;
+const isHas = "car";
+const height = 68;
+
+// if(salary > 20000) {
+//     console.log('you can come to get married')
+// }
+// else {
+//     console.log('you can go way')
+// }
+
+if(salary > 25000 && height > 70 || isBcs == true && isHas == true ) {
+    console.log('you can come to meet with me')
+}
+else {
+    console.log('no need come to meet with me');
+    
+}
