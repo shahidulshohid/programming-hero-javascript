7a4e045b6a4dc009c64fbc79199ecfe4ab86a593
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-30 12:41:44 +0600

    students result figure out is done

---
+++ module18/task.js
@@ -0,0 +1,32 @@
+
+// 1 number task 
+// let burger = 400;
+
+// if(burger > 500){
+//     console.log("Free coke")
+// }
+// else {
+//     console.log("coke: 30tk")
+// }
+
+// 2 number task
+
+// 3 number task
+let score = 95;
+
+if (score >= 90){
+    console.log('you have got A grade')
+}
+else if (score >= 80){
+    console.log('you have got B grade');
+}
+else if (score >= 70){
+    console.log('you have got c grade');
+}
+else if(score >= 60){
+    console.log('you have got d grade');
+}
+else{
+    console.log('you have got f grade');
+    
+}
