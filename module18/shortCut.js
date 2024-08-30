80b822fd9a42d69419241660cc5fb5cb53abb39d
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-30 12:12:04 +0600

    module18 practice is done

---
+++ module18/shortCut.js
@@ -0,0 +1,15 @@
+let isLeader = true;
+// if(isLeader){
+//     console.log("leader astase sobai chare saro");
+// }
+// else {
+//     console.log("leader k dhukte deba na");
+    
+// }
+if(!isLeader){
+    console.log('take somman koro');
+}
+else {
+    console.log('take somman korar dorkar nai');
+    
+}
