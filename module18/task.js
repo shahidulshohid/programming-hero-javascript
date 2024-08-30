b7b6cae1459db625e6c457c947087a47ad77e476
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-30 13:01:59 +0600

    4 number task is done

--- module18/task.js
+++ module18/task.js
@@ -12,21 +12,43 @@
 // 2 number task
 
 // 3 number task
-let score = 95;
+// let score = 95;
 
-if (score >= 90){
-    console.log('you have got A grade')
-}
-else if (score >= 80){
-    console.log('you have got B grade');
-}
-else if (score >= 70){
-    console.log('you have got c grade');
-}
-else if(score >= 60){
-    console.log('you have got d grade');
+// if (score >= 90){
+//     console.log('you have got A grade')
+// }
+// else if (score >= 80){
+//     console.log('you have got B grade');
+// }
+// else if (score >= 70){
+//     console.log('you have got c grade');
+// }
+// else if(score >= 60){
+//     console.log('you have got d grade');
+// }
+// else{
+//     console.log('you have got f grade');
+    
+// }
+
+// 4 number task 
+let score = 85;
+let friendScore = 0;
+if(score > 80){
+    if(friendScore > 80){
+        console.log('I will go to have lunch with my friend');
+    }
+    else if(friendScore >= 60){
+        console.log('Good luck next time will have lunch')
+    }
+    else if(friendScore >= 40){
+        console.log('I blocked your message and unseen')
+    }
+    else{
+        console.log('I blocked my friend from today');
+    }
 }
 else{
-    console.log('you have got f grade');
+    console.log("I will go home because, im so sad today");
     
 }
