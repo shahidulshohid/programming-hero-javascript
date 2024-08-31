66c2af885231f9f587ac2f48e0efc927e321d5b1
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-31 10:47:54 +0600

    add and remove last elemebt

---
+++ module18.5/array.js
@@ -0,0 +1,11 @@
+// add and remove last element 
+//============================
+const array = [34, 54, 66, 77, 44];
+console.log(array);
+//use for adding a element after last element by push() method;
+array.push(99);
+console.log(array)
+//use for removing a last element by pop() method;
+array.pop();
+console.log(array)
+
