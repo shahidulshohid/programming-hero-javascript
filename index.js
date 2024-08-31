3ce042ea34857e71a1c22416d9d5a156e6b2382e
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-01 01:48:36 +0600

    slice practice

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-// var name = prompt('what is your name');
-// var num = prompt('what is your favorite number')
-// var heading = document.getElementById("heading")
-// var result = heading.innerText = "dksjdkfj"
-// console.log(result)
-// console.log('Hello Shahidul' + name);
-// console.log(num + 'That is your favorite number');
 
-//for run more than one variables
-// ===========================
-var num1 = "value1";
-var num2 = "value2";
-var num3 = "value3";
-console.table ([num1, num2, num3])
+// //for run more than one variables
+// // ===========================
+// var num1 = "value1";
+// var num2 = "value2";
+// var num3 = "value3";
+// console.table ([num1, num2, num3])
+
+// use slice and splice 
+var bestFriend = ['shahidul', 'al-amin', 'setu', 'shakkhor', 'faruk', 'momin', 'mukter'];
+console.log(bestFriend.slice(2, 6))
