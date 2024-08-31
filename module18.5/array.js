35de3805b6bcbe875994a713e0140cf8da7cda7e
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-31 10:58:32 +0600

    add and remove practice is done

--- module18.5/array.js
+++ module18.5/array.js
@@ -1,11 +1,20 @@
-// add and remove last element 
-//============================
-const array = [34, 54, 66, 77, 44];
-console.log(array);
-//use for adding a element after last element by push() method;
-array.push(99);
-console.log(array)
-//use for removing a last element by pop() method;
-array.pop();
-console.log(array)
+// // add and remove last element 
+// //============================
+// const array = [34, 54, 66, 77, 44];
+// console.log(array);
+// //use for adding a element after last element by push() method;
+// array.push(99);
+// console.log(array)
+// //use for removing a last element by pop() method;
+// array.pop();
+// console.log(array)
 
+// add and remove first element 
+const array = [50, 69, 33, 12, 77];
+console.log(array)
+//use for removing first element by shift() method;
+array.shift()
+console.log(array)
+//use for adding before first element by unshift() method;
+array.unshift(90)
+console.log(array)
