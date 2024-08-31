c7bd585c449af05adba80e66d75b6f43b320e686
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-31 13:29:51 +0600

    includes use is finsh

--- module18.5/task.js
+++ module18.5/task.js
@@ -5,13 +5,17 @@
 // fruits[2] = "jambura";
 // console.log(fruits)
 
-// 2 number task 
-const touristPlace = ['cox-bazar', 'bandarbon', 'khagrasori'];
-touristPlace.push('sundorban');
-touristPlace.push('chiriakhana', 'dana-park', 'momdern-park')
-console.log(touristPlace);
-touristPlace.pop();
-console.log(touristPlace);
+// // 2 number task 
+// const touristPlace = ['cox-bazar', 'bandarbon', 'khagrasori'];
+// touristPlace.push('sundorban');
+// touristPlace.push('chiriakhana', 'dana-park', 'momdern-park')
+// console.log(touristPlace);
+// touristPlace.pop();
+// console.log(touristPlace);
 
+// 3 number task
+const books = ['english', 'bangla', 'math', 'history', 'javascript'];
+console.log(books.includes('javascript')); //true;
+console.log(books.includes('biology')) //false;
 
 
