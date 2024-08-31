7c3fd407d05c5bf32bb37137140b62a0a988b9df
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-31 12:13:24 +0600

    everything practice

--- module18.5/array.js
+++ module18.5/array.js
@@ -9,12 +9,38 @@
 // array.pop();
 // console.log(array)
 
-// add and remove first element 
-const array = [50, 69, 33, 12, 77];
-console.log(array)
-//use for removing first element by shift() method;
-array.shift()
-console.log(array)
-//use for adding before first element by unshift() method;
-array.unshift(90)
-console.log(array)
+// // add and remove first element 
+// const array = [50, 69, 33, 12, 77];
+// console.log(array)
+// //use for removing first element by shift() method;
+// array.shift()
+// console.log(array)
+// //use for adding before first element by unshift() method;
+// array.unshift(90)
+// console.log(array)
+
+// //use for get out element of array by includes;
+// const array = ['shahidul', 'islam', 'babul', 'kabul', 'habul']
+// console.log(array.includes('babul'))
+// console.log(array.includes('shazzad'))
+// //we can use at if else condition like ....
+// if(array.includes("babul")){
+//     console.log('I will go to market with him')
+// }else{
+//     console.log('I will not go there')
+// }
+
+// // //use for get out index number of array by indexOf;
+// const array = [54, 64, 33, 55 ,88];
+// console.log(array.indexOf(88))
+// console.log(array.indexOf(90))
+
+// //use for finding array by Array.isArray() like.............
+// const numbers = [84, 30, 93,85,53];
+// const array = [];
+// const another = "[]";
+// console.log(Array.isArray(numbers));
+// console.log(Array.isArray(array));
+// console.log(Array.isArray(another))
+
+use apps.join() method for doing something;
