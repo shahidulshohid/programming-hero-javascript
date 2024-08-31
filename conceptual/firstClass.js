709cc514425757ec5263de45c3601bd8c5344546
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-31 22:49:53 +0600

    leart about quep

--- conceptual/firstClass.js
+++ conceptual/firstClass.js
@@ -5,10 +5,15 @@
 // var y = false;
 // console.log(!!y);
 
-//flik kora ba ultano false theme true ///true theke false
-var x = false;
-console.log(!x);
+// //flik kora ba ultano false theme true ///true theke false
+// var x = false;
+// console.log(!x);
 
-var y = true;
-console.log(!y)
+// var y = true;
+// console.log(!y)
 
+//square and quep kora
+var x = 3;
+console.log(x ** 2);
+console.log(x ** 5);
+console.log(x ** 3);
