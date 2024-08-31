29dd8462de1efd7bcf2f8a8ef9d33acb29b71b4f
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-31 22:47:09 +0600

    learn some thing about javascript

---
+++ conceptual/firstClass.js
@@ -0,0 +1,14 @@
+
+// //get out truthy value and false value
+// var x = "islam";
+// console.log(!!x);
+// var y = false;
+// console.log(!!y);
+
+//flik kora ba ultano false theme true ///true theke false
+var x = false;
+console.log(!x);
+
+var y = true;
+console.log(!y)
+
