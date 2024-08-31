2604996ecedf77629173bb7e2082c33da7de38ad
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-31 23:29:05 +0600

    practice if else

--- conceptual/firstClass.js
+++ conceptual/firstClass.js
@@ -12,8 +12,17 @@
 // var y = true;
 // console.log(!y)
 
-//square and quep kora
-var x = 3;
-console.log(x ** 2);
-console.log(x ** 5);
-console.log(x ** 3);
+// //square and quep kora
+// var x = 3;
+// console.log(x ** 2);
+// console.log(x ** 5);
+// console.log(x ** 3);
+
+//if else condition
+var isRaining = false;
+if(!isRaining){
+    console.log('will not go outside');
+}
+else {
+    console.log('will go outside')
+}
