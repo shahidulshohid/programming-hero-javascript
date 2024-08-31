a80c9aa44b6e4c98cdc0e08997e076951f722490
shahidul shohid <shahidulislamshohid7gmail.com>
2024-08-31 13:41:17 +0600

    task 5 number is fully done

--- module18.5/task.js
+++ module18.5/task.js
@@ -18,13 +18,18 @@
 // console.log(books.includes('javascript')); //true;
 // console.log(books.includes('biology')) //false;
 
-// 4 number task
-const friends = ['islam', 'abul', 'mokbul', 'kobul'];
-const numbers = [];
-const fruits = '[]';
-console.log(Array.isArray(friends)); //true;
-console.log(Array.isArray(numbers)); //true;
-console.log(Array.isArray(fruits)); //false;
+// // 4 number task
+// const friends = ['islam', 'abul', 'mokbul', 'kobul'];
+// const numbers = [];
+// const fruits = '[]';
+// console.log(Array.isArray(friends)); //true;
+// console.log(Array.isArray(numbers)); //true;
+// console.log(Array.isArray(fruits)); //false;
 
-
+// 5 number task
+const num1 = [2, 4, 5, 9, 2, 7, 8];
+const num2 = [20, 40, 50, 90, 20, 70, 80];
+console.log(num1.concat(num2))
+console.log(num1);
+console.log(num2);
 
