671a264123c4e87dc1ab7f17e605c659054f2110
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-02 00:18:36 +0600

    javascript some practice

--- module19/different.js
+++ module19/different.js
@@ -2,12 +2,62 @@
 // for(let i = 0; i <= 20; i++){
 //     if(i % 2 === 1){
 //         console.log(i);
+//     }
+// }
+
+// for(let i = 0; i <= 20; i++){
+//     if(i % 2 !== 0){
+//         console.log(i)
+//     }
+// }
+
+// give me the list numbers between 1 to 30 divisible by 5 
+//==============================================
+// for(let i = 1; i <= 30; i++){
+//     if(i % 5 === 0){
+//         console.log(i);
         
 //     }
 // }
-// even number 
+
+// for(let i = 0; i <= 30; i++){
+//     if(i % 3 === 0 || i % 5 === 0){
+//         console.log(i);
+//     }
+// }
+
+// for(let i = 0; i <= 30; i++){
+//     if(i % 3 === 0 && i % 5 === 0){
+//         console.log(i);
+//     }
+// }
+
+//give me the sum of numbers from 1 to 20 that are divisible by 3
+let sum = 0;
 for(let i = 0; i <= 20; i++){
-    if(i % 2 === 0){
-        console.log(i);
+    if(i % 3 === 0){
+        sum += i;
+        console.log('total', sum);
     }
 }
+console.log('total of the numbers', sum);
+
+
+// for(let i = 0; i <= 30; i++){
+//     if(i % 3 === 0 || i % 5 === 0){
+//         console.log(i);
+//     }
+//     else{
+//         if(i % 3 !== 0 || i % 5 !== 0){
+//             console.log('don not want', i);
+//         }
+//     }
+// }
+
+// // even number 
+//=============================
+// for(let i = 0; i <= 20; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
