5d7769547b0178cb49fc17becd99adac88e436cd
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-02 02:09:13 +0600

    some js learnt

---
+++ module19/break.js
@@ -0,0 +1,30 @@
+// // use break by for loop
+// for(let i = 0; i <= 40; i++){
+//     console.log(i);
+//     if(i >= 5){
+//         break;
+//     }
+// }
+// console.log('Life after break up');
+
+// use break by while loop 
+// let i = 0;
+// while(i < 50){
+//     console.log(i);
+    
+//     if(i >= 5){
+//         break
+//     }
+//     i++
+// }
+// console.log('Life after break up');
+// again 
+//===============================
+let i = 54;
+while(i > 25){
+    console.log(i);
+    if(i < 50){
+        break;
+    }
+    i--
+}
