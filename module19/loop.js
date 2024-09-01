d64a8a7d6f47a3b1259e85db45de59d1899ed4c6
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-01 20:55:38 +0600

    while loop practice

---
+++ module19/loop.js
@@ -0,0 +1,11 @@
+// for of loop 
+// const numbers = [32, 45, 22, 44, 65, 27, 97];
+// for(const num of numbers){
+//     console.log(num);
+// }
+// while loop
+let count = 1;
+while(count < 5){
+    console.log(count)
+    count ++
+}
