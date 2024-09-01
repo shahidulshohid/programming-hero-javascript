73f77af8f1db2f19244e3aabceebbb364358f578
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-01 22:29:18 +0600

    even number practice

---
+++ module19/even.js
@@ -0,0 +1,17 @@
+// let num = 1;
+// while(num <= 10){
+//     if(num % 2 === 0){
+//         console.log('these are even number', num);
+//     }
+//     num++
+// }
+
+// odd number 
+let num = 1;
+while(num <= 20){
+    if(num % 2 != 0){
+        console.log(num);
+        
+    }
+    num++
+}
--- module19/loop.js
+++ module19/loop.js
@@ -4,8 +4,33 @@
 //     console.log(num);
 // }
 // while loop
-let count = 1;
-while(count < 5){
-    console.log(count)
-    count ++
-}
+// let count = 1;
+// while(count < 5){
+//     console.log(count)
+//     count ++
+// }
+// another approach 
+// let num = 0;
+// while(num <5){
+//     console.log(num);
+//     num ++;
+    
+// }
+
+// let num = 50;
+// while(num <= 100){
+//     console.log(num);
+//     num ++
+// }
+// //===================
+// let num = 1;
+// let sum = 0;
+// while(num <= 10){
+//     console.log(num);
+    
+//     sum = sum + num;
+//     console.log("sum:", sum);
+//     num ++
+// }
+
+
