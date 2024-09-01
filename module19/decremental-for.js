f47230a53e4b9a0305ec857be10ff2603aa48823
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-01 23:48:19 +0600

    some practice is done

---
+++ module19/decremental-for.js
@@ -0,0 +1,10 @@
+// // incremental for 
+// for(let i = 0; i <= 10; i++){
+//     console.log(i);
+    
+// }
+
+// decremental for 
+for(let i = 10; i >= 0; i--){
+    console.log(i);
+}
---
+++ module19/different.js
@@ -0,0 +1,13 @@
+// // odd number 
+// for(let i = 0; i <= 20; i++){
+//     if(i % 2 === 1){
+//         console.log(i);
+        
+//     }
+// }
+// even number 
+for(let i = 0; i <= 20; i++){
+    if(i % 2 === 0){
+        console.log(i);
+    }
+}
