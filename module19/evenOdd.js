d3678e23ca381fbbd0f9625230b47d9c0e23b6a7
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-01 23:07:13 +0600

    odd and even number practice is done

---
+++ module19/evenOdd.js
@@ -0,0 +1,32 @@
+// for loop
+// for(let i = 0; i <= 10; i++){
+//     console.log(i)
+    
+// }
+// even number 
+// for(let i = 0; i <= 20; i = i+2){
+//     console.log(i)
+    
+// }
+// for(let i = 0; i <= 20; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+// odd number 
+// for(let i = 1; i <= 20; i+=2){
+//     console.log(i);
+    
+// }
+// for(let i = 0; i <= 20; i++){
+//     if(i % 2 === 1){
+//         console.log(i);
+//     }
+// }
+for(let i = 0; i <= 20; i++){
+    if(i % 2 != 0){
+        console.log(i);
+        
+    }
+}
