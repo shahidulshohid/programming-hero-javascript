e87ce5b49b7d33f45e7d7a406770172073978982
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-01 17:14:25 +0600

    some practice

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 // //for run more than one variables
 // // ===========================
 // var num1 = "value1";
@@ -6,9 +5,46 @@
 // var num3 = "value3";
 // console.table ([num1, num2, num3])
 
-// use slice and splice 
-var bestFriend = ['shahidul', 'al-amin', 'setu', 'shakkhor', 'faruk', 'momin', 'mukter'];
-// console.log(bestFriend.slice(2, 6));
-console.log(bestFriend.splice(0, 3, 'mahmudullah'));
-console.log(bestFriend);
+// // use slice and splice
+// var bestFriend = ['shahidul', 'al-amin', 'setu', 'shakkhor', 'faruk', 'momin', 'mukter'];
+// // console.log(bestFriend.slice(2, 6));
+// console.log(bestFriend.splice(0, 3, 'mahmudullah'));
+// console.log(bestFriend);
 
+// practice first logical thing from making decision in your code website
+//======================================== one number
+// const select = document.querySelector("select");
+// const para = document.querySelector("p");
+// select.addEventListener("change", weather);
+// function weather() {
+//   const choose = select.value;
+//   if (choose === "sunny") {
+//     para.textContent = "Today is sunny day";
+//   } else if (choose === "rainy") {
+//     para.textContent = "Tody is rainy day";
+//   } else if (choose === "snowing") {
+//     para.textContent = "Tody is snowing day";
+//   } else if (choose === "overcast") {
+//     para.textContent = "Tody is overcast day";
+//   }
+// }
+
+//================================ two number
+const select = document.querySelector('select');
+const para = document.querySelector('p');
+select.addEventListener('change', weather);
+const temperature = 86;
+function weather (){
+    const choose = select.value;
+    if(choose === 'sunny'){
+        if(temperature < 86){
+            para.textContent = `It is ${temperature} degrees outside — nice and sunny. Let's go out to the beach, or the park, and get an ice cream.`;
+        }
+        else if(temperature >= 86){
+            para.textContent = `It is ${temperature} degrees outside — REALLY HOT! If you want to go outside, make sure to put some sunscreen on.`;
+        }
+    }
+    else {
+        para.textContent = `There is no set like this condition here like ======== ${choose}`
+    }
+}
