62d27bf754e5fa0c25d5dd3cb0643ee00b3fea8e
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-02 15:21:40 +0600

    practice task is done something

---
+++ module19/tasks/while/odd-even-while.js
@@ -0,0 +1,34 @@
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+/*programming hero*/
+// odd number 
+// let i = 61;
+// while(i <= 100){
+//     if(i % 2 === 1){
+//         console.log('odd number', i)
+//     }
+//     i++
+// }
+// even number 
+let i = 78;
+while(i <= 98){
+    if(i % 2 === 0){
+    console.log('even number', i);
+    }
+    i++
+}
---
+++ module19/tasks/while/sum-of-odd-numbers-while.js
@@ -0,0 +1,45 @@
+
+
+/***
+
+Subtask-1:
+
+Display sum of all the odd numbers from 81 to 131.
+
+ */
+/***
+
+Subtask-2:
+
+Display sum of all the even numbers from 206 to 311.
+
+ */
+
+/*programming hero*/
+// sum odd number 
+// let i = 81;
+// let sum = 0;
+// while(i <= 131){
+//     console.log('these are regular number', i);
+    
+//     if(i % 2 === 1){
+//         sum = sum + i;
+//         console.log('these are sum number', sum);
+//     }
+//     i++
+// }
+// console.log('these are total sum number', sum);
+
+// even number 
+let i = 206;
+let sum = 311;
+while(i <= 311){
+    console.log('these are regular number', i);
+    
+    if(i % 2 === 0){
+        sum = sum + i;
+        console.log('these are sum number', sum);
+    }
+    i++
+}
+console.log('these are total sum number', sum);
