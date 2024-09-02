5ca6235aa7a179b8f788ed0499551118dc71d0ff
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-02 15:57:14 +0600

    some prctice with multiplication for loop

--- module19/tasks/for/multiplication-table.js
+++ module19/tasks/for/multiplication-table.js
@@ -6,3 +6,9 @@ Generate a multiplication table for number 9
 
 
 /*programming hero*/
+// for loop multiplication 
+let number = 9;
+for(let i = 1; i <= 10; i++){
+    console.log(`${number} X ${i} = ${number * i}`);
+    
+}
