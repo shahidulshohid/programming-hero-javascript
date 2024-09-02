d79115e81db63bbedbda02c19047ff5a5488923a
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-02 14:57:26 +0600

    even number practice is done

--- module19/tasks/for/sum-of-odd-numbers.js
+++ module19/tasks/for/sum-of-odd-numbers.js
@@ -16,3 +16,23 @@ Display sum of all the even numbers from 51 to 85.
  */
 
 /*programming hero*/
+// for loop to sum odd 
+// let sum = 0;
+// for(let i = 91; i <= 129; i++){
+//     console.log( 'these are regular number', i)
+//     if(i % 2 === 1){
+//         sum = sum + i;
+//         console.log('these are sum:', sum)
+//     }
+// }
+
+// for loop to sum even 
+let sum = 0;
+for(let i = 51; i <= 85; i++){
+    console.log('these are regular number', i);
+    if(i % 2 === 0){
+        sum = sum + i;
+        console.log('these are even number', sum);
+    }
+}
+console.log('total', sum);
