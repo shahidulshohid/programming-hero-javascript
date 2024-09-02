8f55e39271a191db2bc2e71cf6534d4cd265d925
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-02 22:44:32 +0600

    i learnt string and array

---
+++ module20/string.js
@@ -0,0 +1,17 @@
+const country = "bangladesh";
+const division = 'rajshahi';
+const district = 'bogura';
+const thana = new String('shahjahanpur')
+// console.log(thana)
+
+const numbers = [2, 4, 56, 6, 4, 7];
+// console.log(numbers.length)
+// console.log(district.length)
+// console.log(numbers[1])
+// console.log(district[1])
+console.log(numbers)
+numbers[0] = 40;
+console.log(numbers)//array is a changeable data type that's mean's mutable data type;
+console.log(district)
+district[0] = 'f';
+console.log(district)
