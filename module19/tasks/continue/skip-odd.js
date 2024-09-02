1320fb4a33f4fc68ad5d4d2a06548ac798943b54
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-02 14:26:48 +0600

    practice odd nuber by continue

--- module19/tasks/continue/skip-odd.js
+++ module19/tasks/continue/skip-odd.js
@@ -1,3 +1,19 @@
 /* 
 Write a loop to print even numbers from 1 to 40. Use continue to skip odd numbers.
 */
+// for loop
+// even number 
+// for(let i = 1; i <= 40; i++){
+//     if(i % 2 === 0){
+//         console.log(i);
+//     }
+// }
+
+// use continue to skip odd numbers 
+for(let i = 1; i <= 40; i++){
+    if(i % 2 === 1){
+        continue;
+    }
+    console.log(i);
+    
+}
