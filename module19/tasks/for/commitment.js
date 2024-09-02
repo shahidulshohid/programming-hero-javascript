d3b11de50afc67b2e3441f4fd542fcdb5587e705
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-02 16:16:20 +0600

    today practice done

--- module19/tasks/for/commitment.js
+++ module19/tasks/for/commitment.js
@@ -5,7 +5,8 @@
  */
 
 /*programming hero*/
-for(let i = 1; i <= 60; i++){
-    console.log(`I will invest at least 6 hrs every single day for next 60 days! ${i}`);
+// for loop 
+// for(let i = 1; i <= 60; i++){
+//     console.log(`I will invest at least 6 hrs every single day for next 60 days! ${i}`);
     
-}
+// }
---
+++ module19/tasks/while/multiplication-table-while.js
@@ -0,0 +1,14 @@
+/***
+
+As Ersa is learning now, she wants to explore more and more. Tell Ersa to generate a multiplication table for number 5
+
+ */
+
+/*programming hero*/
+// multiplication while loop
+// let number = 9;
+// let i = 1;
+// while (i <= 10) {
+//   console.log(`${number} X ${i} = ${number * i}`);
+//   i++;
+// }
