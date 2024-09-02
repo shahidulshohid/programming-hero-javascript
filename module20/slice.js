09cfceaeb54db4e5e03a4d50dad7c39806efd073
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-03 02:13:37 +0600

    use includes

---
+++ module20/slice.js
@@ -0,0 +1,6 @@
+// const address = 'andorkilla';
+// const part = address.slice(6,9);
+// console.log(part);
+const country = 'bangladesh';
+const part = country.slice(0, 5)
+console.log(part);
---
+++ module20/split.js
@@ -0,0 +1,35 @@
+// use split() 
+//======================
+// const sentence = 'i am a good and hardworking person';
+// console.log(sentence.split('a'));
+//======================
+// const friendStr = 'rahim,karim,alamin';
+// const friends = friendStr.split(',');
+// console.log(friends);
+
+// use join ()
+//========================
+// const array =  ['shahidul', 'islam', 'alamin', 'akbar'];
+// console.log(array.join('-'))
+
+// // use concat()
+// const first = 'shhidul'
+// const last = 'islam'
+// const fullName = first + ' ' + last
+// console.log(fullName);
+
+// another 
+// const fullName = first.concat(last)
+// console.log(fullName);
+
+// // another 
+// const fullName = first.concat(' ').concat(last)
+// console.log(fullName)
+
+// use includes 
+// ==================
+// const name = 'shahidul islam';
+// console.log(name.includes('s'))
+// console.log(name.includes('w'))
+
+
