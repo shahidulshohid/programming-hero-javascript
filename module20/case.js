664a435af8b2e7433ba522d961abb737dff670bc
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-02 23:46:16 +0600

    string

---
+++ module20/case.js
@@ -0,0 +1,36 @@
+// const school = "Uttara model Shcool";
+// console.log(school.toUpperCase());
+// console.log(school.toLowerCase());
+//=================================
+// const subject = 'Chemistry';
+// const book = 'chemistry';
+// if(subject === book){
+//     console.log('i am reading book aibar porikkhai fatai felmu');
+// }
+// else{
+//     console.log('hudai hudai prista ultai aibar khawyar jonno');
+// }
+//====================================
+// if(subject.toLocaleLowerCase()===book.toLocaleLowerCase()){
+//     console.log('i am reading book aibar porikkhai fatai felmu');
+// }
+// else{
+//     console.log('hudai hudai prista ultai aibar khawyar jonno');
+// }
+//==============================
+const drink = '   water';
+const liquid = 'water   '
+// if(drink === liquid){
+//     console.log('pani er opor nam life');
+// }
+// else{
+//     console.log('sumodre pani ase khaite pari na');
+// }
+//===============================
+if(drink.trim()===liquid.trim()){
+    console.log('prni opor nam life');
+}
+else{
+    console.log('sumodre pani ase khaite pari na');
+    
+}
