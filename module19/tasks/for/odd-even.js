7152d51a592957dde678394103cdf0cbd49a0b15
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-02 14:43:26 +0600

    some practice is done

--- module19/tasks/for/odd-even.js
+++ module19/tasks/for/odd-even.js
@@ -5,6 +5,11 @@ Subtask-1:
 Find all the odd numbers from 61 to 100.
 
  */
+// for(let i = 61; i <= 100; i++){
+//     if(i % 2 === 1){
+//         console.log(i)
+//     }
+// }
 
 
 /***
@@ -14,5 +19,10 @@ Subtask-2:
 Find all the even numbers from 78 to 98.
 
  */
+for(let i = 78; i <= 98; i++){
+    if(i % 2 === 0){
+        console.log(i)
+    }
+}
 
 /*programming hero*/
