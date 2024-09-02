6116e0304f67f33941e58ee2887bc511183fbb86
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-03 03:32:11 +0600

    practice object

---
+++ module20/object.js
@@ -0,0 +1,16 @@
+const bottle = {
+    brand:'apple',
+    price:45,
+    color:'white',
+    isClean:'false'
+}
+const subject = {
+    name:'biology',
+    teacher:'anita mam',
+    examDate: '30 january',
+    chapters: ['first', 'second', 'third'],
+    exam: {
+        name:'final exam',
+        marks:'100'
+    }
+}
