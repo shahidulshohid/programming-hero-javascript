19c4bee7d324335abbc3b1bc3b9709e1854981da
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-03 19:21:08 +0600

    task three is done

--- module20/tasks/tasks.js
+++ module20/tasks/tasks.js
@@ -12,10 +12,24 @@
 // ### Task-2
 // For this object below add a property named `passenger capacity` with value 5
 // ```js
-const car = {
-    make: "Toyota",
-    model: "Corolla",
-    year: 2020
+// const car = {
+//     make: "Toyota",
+//     model: "Corolla",
+//     year: 2020
+// };
+// car['passenger capacity'] = 5
+// console.log(car)
+//===============================
+// ### Task-3
+// Display the physics marks as output.
+// ```js
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
+    }
 };
-car['passenger capacity'] = 5
-console.log(car)
+console.log(student.physics.marks);
