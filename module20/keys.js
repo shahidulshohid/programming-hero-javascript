dcc2f71622dce104fac973a865b89de3b065a4fb
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-03 12:37:10 +0600

    keys function ue

---
+++ module20/keys.js
@@ -0,0 +1,11 @@
+const computer = {
+    name: 'lenovo',
+    price:'35000',
+    processor:'intel',
+    hdd:'512gb',
+    monitor:'hp'
+}
+// console.log(computer)
+const keys = Object.keys(computer);
+console.log(keys);
+
