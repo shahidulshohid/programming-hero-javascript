30a3b92b0c02a5ca96ff0ecd2e1da5296df6c913
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-03 19:19:17 +0600

    task one and task two is done

---
+++ module20/tasks/tasks.js
@@ -0,0 +1,21 @@
+// ### Task-1
+// Access the `golden rod` color value in output.
+// ```
+// const colors = {
+//     red: "#ff0000",
+//     green: "#00ff00",
+//     blue: "#0000ff",
+//     "golden rod": '#daa520'
+// };
+// console.log(colors["golden rod"])
+//==========================
+// ### Task-2
+// For this object below add a property named `passenger capacity` with value 5
+// ```js
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+car['passenger capacity'] = 5
+console.log(car)
