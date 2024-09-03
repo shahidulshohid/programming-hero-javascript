9bd42ee3cac9420dc53cf4c0e1f6995e34529663
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-03 19:55:03 +0600

    four and five number tasks are done

--- module20/tasks/tasks.js
+++ module20/tasks/tasks.js
@@ -8,6 +8,7 @@
 //     "golden rod": '#daa520'
 // };
 // console.log(colors["golden rod"])
+
 //==========================
 // ### Task-2
 // For this object below add a property named `passenger capacity` with value 5
@@ -19,17 +20,55 @@
 // };
 // car['passenger capacity'] = 5
 // console.log(car)
+
 //===============================
 // ### Task-3
 // Display the physics marks as output.
 // ```js
-const student = {
-    name: "Hamim Sakep",
-    id: 5421,
-    physics: {
-        subject: "HSC Physics",
-        author: "Shahjahan Tapan",
-        marks: 30
-    }
+// const student = {
+//     name: "Hamim Sakep",
+//     id: 5421,
+//     physics: {
+//         subject: "HSC Physics",
+//         author: "Shahjahan Tapan",
+//         marks: 30
+//     }
+// };
+// console.log(student.physics.marks);
+
+//================================
+// ### Task-4
+
+// Count the `number of properties`.
+
+// **Input:**
+// <br>
+// ```js
+// let student = {
+//     name: 'Ariana Grande',
+//     age: 21,
+//     city: 'Gaibandha',
+//     isStudent: true
+// };
+// const propertiesNumber = Object.keys(student).length;
+// console.log(propertiesNumber);
+
+// //====================================
+// ### Task-5 (Hard)
+
+// Loop through an object and print the key-value pairs with their `types`
+
+// **Input:**
+// <br>
+
+let myObject = {
+
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+
 };
-console.log(student.physics.marks);
+for(const key in myObject){
+    console.log(`key: ${key} | type: ${ typeof myObject[key]}`)
+}
