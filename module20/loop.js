f0e31d6419d6f95d2013c434fbcee6b1034b320a
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-03 13:49:17 +0600

    new object use

---
+++ module20/loop.js
@@ -0,0 +1,30 @@
+const mobile = {
+    brand:'samsung',
+    price:25000,
+    color:'black',
+    camera:'12mp',
+    isNew:'true'
+}
+// console.log(mobile);
+// for(const prop in mobile){
+//     console.log(prop);
+//     console.log(mobile[prop])
+// }
+
+// const keys = Object.keys(mobile)
+// console.log(keys);
+// for(const key of keys){
+//     console.log(key)
+//     console.log(mobile[key])
+// }
+// const values = Object.values(mobile);
+// for(const value of values){
+//     console.log(value)
+// }
+//=====================
+// const person = new Object({name:"shaidul", profession:'devaelopr'})
+// console.log(person);
+//======================
+const car = Object.create({})
+console.log(car);
+
