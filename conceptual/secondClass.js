68072cd4bcd2e40e430c15eccac94cfd1b27d296
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-05 01:07:12 +0600

    some practice

--- conceptual/secondClass.js
+++ conceptual/secondClass.js
@@ -6,20 +6,31 @@
 // console.log(numbers)
 // const newNumber = numbers.slice(2, 5)
 // console.log(newNumber);
-
-const fruits = ['amm','jam','katal','lichi']
-const ful = ['shapla','rogonigondha','lili','joba','poddo']
-const numbers = [23,44,3,65,77,90]
+//===================================
+// const fruits = ['amm','jam','katal','lichi']
+// const ful = ['shapla','rogonigondha','lili','joba','poddo']
+// const numbers = [23,44,3,65,77,90]
 // console.log(fruits);
 // console.log(ful);
 // console.log(numbers)
 // const akakar = fruits.concat(ful, numbers)
 // console.log(akakar);
 // const newFruits = fruits.indexOf('amm')
-const find = fruits.includes('kamranga')
-console.log(find);
+// const find = fruits.includes('kamranga')
+// console.log(find);
 // const checkArray = Array.isArray(ful)
 // console.log(checkArray);
+//===============================
+// const flowers = ['galap', 'joba', 'lili', 'shalpa'];
+// const addFlowers = flowers.join(' ')
+// console.log(typeof addFlowers)
+//================================
+const string = "galap,joba,lili,shalpa";
+const convert = string.split(",")
+console.log(convert)
+// console.log(typeof convert);
+console.log(Array.isArray(convert))
+
 
 
 
