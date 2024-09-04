8d9fe04ffe20fea4db93b90c541e594a3bb1e49f
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-04 15:22:49 +0600

    reverse array by while loop

--- array-module20.5/tasks/task.js
+++ array-module20.5/tasks/task.js
@@ -10,13 +10,25 @@
 // `['orange', 'yellow', 'green', 'blue', 'red']`
 // for loop 
 // ===================
-const colors = ['red', 'blue', 'green', 'yellow', 'orange']
-const rev_colors = [];
-for(let i = colors.length -1; i >= 0; i--){
-    const color = colors[i];
-    rev_colors.push(color);
-}
-console.log(rev_colors);
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+// const rev_colors = [];
+// for(let i = colors.length -1; i >= 0; i--){
+//     const color = colors[i];
+//     rev_colors.push(color);
+// }
+// console.log(rev_colors);
 
 // while loop 
 //=======================
+const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+const rev_colors = [];
+let i = 0;
+while(i < colors.length){
+    const color = colors[i]
+    rev_colors.unshift(color)
+    i++
+}
+console.log(rev_colors);
+
+
+
