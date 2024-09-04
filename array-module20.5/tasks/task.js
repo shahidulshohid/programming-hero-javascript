2c8b7eab160acb1c13b310d14495483c5a370077
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-04 17:39:40 +0600

    task 5 is done

--- array-module20.5/tasks/task.js
+++ array-module20.5/tasks/task.js
@@ -54,17 +54,72 @@
 // console.log(newNumbers);
 //=========================
 // while loop
-const numbers = [12, 98, 5, 41, 23, 78, 46];
-const newNumbers = [];
-let i = 0;
-while(i < numbers.length){
-    if(numbers[i] % 2 === 0){
-        const num = numbers[i];
-        newNumbers.push(num)
-    }
-    i ++
+// const numbers = [12, 98, 5, 41, 23, 78, 46];
+// const newNumbers = [];
+// let i = 0;
+// while(i < numbers.length){
+//     if(numbers[i] % 2 === 0){
+//         const num = numbers[i];
+//         newNumbers.push(num)
+//     }
+//     i ++
+// }
+// console.log(newNumbers);
+//=======================
+// ### Task 3
+
+// Use a for...of loop to concatenate all the elements of an array into a single string.
+
+// **Input:**
+// `var numbers = ['Tom', 'Tim', 'Tin', 'Tik']`
+
+// **Output:**
+
+// `'TomTimTinTik'`
+//============================
+// for of loop
+// const numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
+// let result = '';
+// for(const num of numbers){
+//     result = result + num;
+// }
+// console.log(result);
+//==================
+//for loop
+// const numbers = ['Tom', 'Tim', 'Tin', 'Tik'];
+// let result = "";
+// for(let i = 0; i < numbers.length; i++){
+//     result += numbers[i];
+// }
+// console.log(result);
+
+//=========================
+// ### Task 4 (Hard)
+
+// Reverse the words of a sentence. Only the position of the word will be reversed. check out the output
+
+// **Input:**
+// `const statement = 'I am a hard working person'`
+
+// **Output:**
+
+// `'person working hard a am I'`
+//===================
+// const statement = 'I am a hard working person'
+// const reverseStatement = statement.split(' ').reverse().join(' ')
+// console.log(reverseStatement);
+
+//============================
+const statement = 'I am a hard working person'
+const newStatement = statement.split(' ');
+let makingStr = []
+// console.log(newStatement.reverse());
+for(let word of newStatement){
+    makingStr.unshift(word);
 }
-console.log(newNumbers);
+console.log(makingStr.join(' '));
+
+
 
 
 
