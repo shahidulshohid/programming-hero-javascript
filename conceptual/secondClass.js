2177d385eaf55eeaacf19f76ce185810dd46dab2
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-05 00:42:44 +0600

    i am learning array

---
+++ conceptual/secondClass.js
@@ -0,0 +1,26 @@
+// const numbers = [23, 45,23, 65, 7, 88];
+// numbers.push(100);
+// numbers.pop()
+// numbers.unshift(50)
+// numbers.shift()
+// console.log(numbers)
+// const newNumber = numbers.slice(2, 5)
+// console.log(newNumber);
+
+const fruits = ['amm','jam','katal','lichi']
+const ful = ['shapla','rogonigondha','lili','joba','poddo']
+const numbers = [23,44,3,65,77,90]
+// console.log(fruits);
+// console.log(ful);
+// console.log(numbers)
+// const akakar = fruits.concat(ful, numbers)
+// console.log(akakar);
+// const newFruits = fruits.indexOf('amm')
+const find = fruits.includes('kamranga')
+console.log(find);
+// const checkArray = Array.isArray(ful)
+// console.log(checkArray);
+
+
+
+
