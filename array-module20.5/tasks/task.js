60930093a8d80f5f287b7e15cecab252ccaf9d50
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-04 15:30:19 +0600

    create a new array

--- array-module20.5/tasks/task.js
+++ array-module20.5/tasks/task.js
@@ -20,15 +20,38 @@
 
 // while loop 
 //=======================
-const colors = ['red', 'blue', 'green', 'yellow', 'orange']
-const rev_colors = [];
-let i = 0;
-while(i < colors.length){
-    const color = colors[i]
-    rev_colors.unshift(color)
-    i++
+// const colors = ['red', 'blue', 'green', 'yellow', 'orange']
+// const rev_colors = [];
+// let i = 0;
+// while(i < colors.length){
+//     const color = colors[i]
+//     rev_colors.unshift(color)
+//     i++
+// }
+// console.log(rev_colors);
+
+//==============================
+// ### Task 2
+
+// Write a JavaScript code to get the even numbers from an array using any looping technique.
+
+// **Input:**
+// `const numbers = [12, 98, 5, 41, 23, 78, 46];`
+
+// **Output:**
+
+// `[12, 98, 78, 46]`
+//==========================
+// for loop
+const numbers = [12, 98, 5, 41, 23, 78, 46];
+const newNumbers = [];
+for(let i = 0; i < numbers.length; i++){
+    if(numbers[i] % 2 === 0){
+        const num = numbers[i];
+        newNumbers.push(num)
+    }
 }
-console.log(rev_colors);
+console.log(newNumbers);
 
 
 
