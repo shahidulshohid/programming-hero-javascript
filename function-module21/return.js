46b810694de6b02196af6c9c16a8fa1926bce0a6
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-06 01:42:19 +0600

    argument leart

---
+++ function-module21/return.js
@@ -0,0 +1,19 @@
+// function tenTimes(number){
+//     const result = number * 10;
+//     return result;
+// }
+// tenTimes(5)
+// const output = tenTimes(5);
+// console.log('output', output);
+
+//===============================
+function cutHalf(number){
+    const result = number / 2;
+    return result;
+}
+cutHalf(10);
+const output = cutHalf(10)
+console.log(output);
+const bigNumber = cutHalf(50)
+console.log(bigNumber);
+
