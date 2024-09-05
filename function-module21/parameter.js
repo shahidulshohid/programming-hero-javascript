30d70603f3fb9bc4f4746fdb6ce5775e8f37c1bd
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-06 01:29:50 +0600

    fun prac is done

--- function-module21/parameter.js
+++ function-module21/parameter.js
@@ -1,18 +1,18 @@
 
-function doubleIt(number){
-    const doubled = number * 2;
-    console.log(number, doubled);
-}
-console.log('I will call the function');
-doubleIt(15)
-console.log('----------------------');
-doubleIt(88);
-console.log('----------------------------')
-doubleIt(873)
-const number = 55;
-doubleIt(number);
-const money = 112;
-doubleIt(money)
+// function doubleIt(number){
+//     const doubled = number * 2;
+//     console.log(number, doubled);
+// }
+// console.log('I will call the function');
+// doubleIt(15)
+// console.log('----------------------');
+// doubleIt(88);
+// console.log('----------------------------')
+// doubleIt(873)
+// const number = 55;
+// doubleIt(number);
+// const money = 112;
+// doubleIt(money)
 //============================
 // function difference(num1, num2){
 //     const diff = num1 + num2;
@@ -22,5 +22,14 @@ doubleIt(money)
 // const myAge = 10;
 // difference(fatherAge, myAge)
 // //===================================
-
+function difference(num1, num2){
+    const diff = num1 + num2;
+    console.log(num1, num2, 'difference is', diff)
+}
+const fatherAge = 40;
+const myAge = 10;
+difference(fatherAge, myAge)
+const motherAge = 35;
+const grandMotherAge = 70;
+difference(motherAge, grandMotherAge)
 
