0766f9addf25671b67bae334ac145324155036ea
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-06 14:59:06 +0600

    one number task is done

---
+++ fun-module21/tasks/task.js
@@ -0,0 +1,10 @@
+// ### Task-1  
+// Take four parameters. Multiply the four numbers and then return the result 
+function fourParamMultiply(num1, num2, num3, num4){
+    const result = num1 * num2 * num3 * num4;
+    return result;
+    
+}
+const totalResult = fourParamMultiply(10, 20, 30, 40);
+console.log(totalResult);
+
