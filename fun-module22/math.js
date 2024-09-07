8133918607a2c1f5a9d4a886d8db0bae2407184e
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-07 16:35:44 +0600

    practice is math

---
+++ fun-module22/math.js
@@ -0,0 +1,21 @@
+// smallest number 
+const min = Math.min(2, 4, 3, 6, 7, 8, 5, 9, 8,9)
+console.log(min)
+//===================
+// boro number 
+const max =Math.max(23,4,5,6,7,8,554)
+console.log(max)
+//==================
+//ata dile akta number dey amader
+console.log(Math.PI)
+//================
+//if we use Math.abs(). then we will get a absolute number like.... below the example
+console.log(Math.abs(5-10)) //here we will get number without the (-) symbol
+//=====================================
+console.log(Math.round(5.45))//it will be output 5. because of the pont number is approximately 5;
+console.log(Math.round(5.60))//it will be output 6. because of the pont number is approximately 6
+//===========================
+console.log(Math.floor(5.95))//it always gives us the result without point number;
+//===============================
+console.log(Math.ceil(3.23)) //it always gives us the number add point number (point number = +1)
+
