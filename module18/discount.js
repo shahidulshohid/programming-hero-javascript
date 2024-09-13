ea26b93ccd151688dea2c1e16d01ba8dc6451550
shahidul shohid <shahidulislamshohid7gmail.com>
2024-09-13 19:32:06 +0600

    just practice here

--- module18/discount.js
+++ module18/discount.js
@@ -1,4 +1,4 @@
-const price = 5000;
+// const price = 5000;
 
 // if(price >= 5000){
 //     //will get discount 10%
@@ -11,20 +11,38 @@ const price = 5000;
     
 // }
 
-if(price >= 8000){
-    //will get 20%
+// if(price >= 8000){
+//     //will get 20%
+//     const discount = price * 20 / 100;
+//     const payAmount = price - discount;
+//     console.log(payAmount);
+// }
+// else if(price > 2500) {
+//     //will discount 5%
+//     const discount = price * 5 /100;
+//     const payAmount = price - discount;
+//     console.log(payAmount);
+    
+// }
+// else {
+//     console.log('you will not get any discount here');
+    
+// }
+
+
+//===================================
+// practice here 
+var price = 9000
+if(price >= 5000){
     const discount = price * 20 / 100;
     const payAmount = price - discount;
     console.log(payAmount);
 }
-else if(price > 2500) {
-    //will discount 5%
-    const discount = price * 5 /100;
+else if (price >= 25000){
+    const discount = price * 50 /100;
     const payAmount = price - discount;
     console.log(payAmount);
-    
 }
 else {
-    console.log('you will not get any discount here');
-    
+    console.log('you will not get any discount here')
 }
